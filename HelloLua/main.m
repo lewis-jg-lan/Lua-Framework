9b3679fc3d0c1937ff5abb8e26ef1b792d30713c
Rhody Lugo <rhodylugo@gmail.com>
2015-05-05 18:38:44 -0430

    skip loading the HelloLua UI when running the tests

--- HelloLua/main.m
+++ HelloLua/main.m
@@ -7,7 +7,18 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
-	return NSApplicationMain(argc, argv);
+
+	@autoreleasepool {
+		[NSApplication sharedApplication];
+		/* Load the main UI if not running the tests */
+		if(NSClassFromString(@"XCTestCase") == nil) {
+			[[NSBundle mainBundle] loadNibNamed:@"MainMenu" owner:NSApp topLevelObjects:nil];
+		}
+		[NSApp run];
+	};
+
+	return 0;
 }
