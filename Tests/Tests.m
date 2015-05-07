8b1d2edee36778057df7e6c66dcb77616ff66b04
Developer <>
2015-05-07 02:04:48 -0430

    add test to retrieve values

--- Tests/Tests.m
+++ Tests/Tests.m
@@ -47,6 +47,63 @@
 	XCTAssertTrue([obj class] == NSClassFromString(@"AVPlayerView"), @"Couldn't create an object from a loaded framework");
 }
 
+- (void)testRetrievingValues {
+	NSNumber *result = [[self.sharedLuaContext evaluateScript:
+						 @"anIntNumber = 55\n"
+						 @"aNegativeIntNumber = -15\n"
+
+						 @"aFloatNumber = 25.33\n"
+						 @"aNegativeFloatNumber = -45.25\n"
+
+						 @"aTrueBooleanValue = true\n"
+						 @"aFalseBooleanValue = false\n"
+
+						 @"aStringWithANumber = '1.11e5'\n"
+
+						 @"year2000InEpochTime = 946684800\n"
+
+						 @"aTable = {color='blue', number=2}\n"
+
+						 @"return anIntNumber + aFloatNumber\n"] toObject];
+
+	XCTAssertTrue([[self.sharedLuaContext[@"anIntNumber"] toObject] intValue] == 55);
+	XCTAssertTrue([[self.sharedLuaContext[@"aNegativeIntNumber"] toObject] intValue] == -15);
+
+	XCTAssertTrue([[self.sharedLuaContext[@"aFloatNumber"] toObject] floatValue] == 25.33f);
+	XCTAssertTrue([[self.sharedLuaContext[@"aNegativeFloatNumber"] toObject] floatValue] == -45.25f);
+
+	XCTAssertTrue([[self.sharedLuaContext[@"aTrueBooleanValue"] toObject] boolValue] == YES);
+	XCTAssertTrue([[self.sharedLuaContext[@"aFalseBooleanValue"] toObject] boolValue] == NO);
+
+	XCTAssertTrue([result floatValue] == 55 + 25.33f);
+
+	XCTAssertTrue([self.sharedLuaContext[@"anIntNumber"] toInt32] == 55);
+	XCTAssertTrue([self.sharedLuaContext[@"aNegativeIntNumber"] toInt32] == -15);
+	XCTAssertTrue([self.sharedLuaContext[@"anIntNumber"] toUInt32] == 55);
+
+	XCTAssertTrue([self.sharedLuaContext[@"aFloatNumber"] toDouble] == 25.33);
+	XCTAssertTrue([self.sharedLuaContext[@"aNegativeFloatNumber"] toDouble] == -45.25);
+
+	XCTAssertTrue([self.sharedLuaContext[@"aTrueBooleanValue"] toBool] == YES);
+	XCTAssertTrue([self.sharedLuaContext[@"aFalseBooleanValue"] toBool] == NO);
+
+	XCTAssertTrue([[self.sharedLuaContext[@"anIntNumber"] toNumber] intValue] == 55);
+	XCTAssertTrue([[self.sharedLuaContext[@"aFloatNumber"] toNumber] floatValue] == 25.33f);
+	XCTAssertTrue([[self.sharedLuaContext[@"aStringWithANumber"] toNumber] floatValue] == 1.11e5f);
+
+	XCTAssertTrue([[self.sharedLuaContext[@"anIntNumber"] toString] isEqualToString:@"55"]);
+	XCTAssertTrue([[self.sharedLuaContext[@"aFloatNumber"] toString] isEqualToString:@"25.33"]);
+	XCTAssertTrue([[self.sharedLuaContext[@"aStringWithANumber"] toString] isEqualToString:@"1.11e5"]);
+
+	NSDateComponents *comps = [[NSDateComponents alloc] init];
+	comps.day = 1;
+	comps.month = 1;
+	comps.year = 2000;
+
+	XCTAssertEqualWithAccuracy([[self.sharedLuaContext[@"year2000InEpochTime"] toDate] timeIntervalSinceReferenceDate],
+							   [[[NSCalendar currentCalendar] dateFromComponents:comps] timeIntervalSinceReferenceDate], 60*60*24);
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
